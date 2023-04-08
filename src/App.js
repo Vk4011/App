4cd2c11bb64cc90119fa8d7ba5193672fb48be1f
G VIKRAM <97166893+Vk4011@users.noreply.github.com>
2023-04-08 21:48:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,15 +47,21 @@ window.addEventListener('load', () => {
   form.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      const task = input.value;
+      if (input.value !== '') {
+        const task = input.value;
 
-      const task_el = document.createElement('div');
-      task_el.classList.add('task');
-
-      const task_content_el = document.createElement('div');
-      task_content_el.classList.add('content');
-
-      task_el.appendChild(task_content_el);
+        const task_el = document.createElement('div');
+        task_el.classList.add('task');
+  
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('content');
+  
+        task_el.appendChild(task_content_el);
+  
+      }
+      else{
+        console.log('cant add empty list');
+      }
 
       const task_input_el = document.createElement('input');
       task_input_el.classList.add('text');
